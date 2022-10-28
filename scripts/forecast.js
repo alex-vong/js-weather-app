769883b28b61281f341e4ee3282d9a775b3de912
Alex Vong <avong003@gmail.com>
2022-10-28 14:42:55 -0700

    created a function to get city information from input value

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,7 +1,60 @@
-window.addEventListener('load', (event) => {
-  console.log('page is fully loaded');
 
 
-});
+const key ='iUmMcAAGsoeLLp9NLpN3HWtMFkgws4K3';
+
+//get weather information
+
+const getWeather = async (id) => {
+
+	const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+	const query = `${id}?apikey=${key}`;
+
+	const response = await fetch(base + query);
+
+	const data = await response.json();
+
+	// console.log(data);
+	// console.log(data[0].WeatherText);
+
+	return data[0];
+
+};
+
+
+
+
+
+
+
+
+// get city information
+const getCity = async (city) => {
+
+	const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+	//when adding query parameters we need to start with ? and use & when we query another one on it
+	const query = `?apikey=${key}&q=${city}`;
+
+	const response = await fetch(base + query);
+
+	const data = await response.json();
+
+	// console.log(data[0]);
+	return data[0];
+
+};
+
+// getCity('cypress')
+// 	.then(data => {
+// 		return getWeather(data.Key);
+// 	}).then(data => {
+// 		console.log(data);
+// 	}).catch (error => console.log(error));
+
+
+
+
+
+
+
 
 
