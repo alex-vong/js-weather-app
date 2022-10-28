e05b0ad0e795dcdafa1106841a58d833172d63d0
Alex Vong <avong003@gmail.com>
2022-10-28 14:43:42 -0700

    added event listener to input to take value and get city detail and weather details

--- scripts/app.js
+++ scripts/app.js
@@ -1,15 +1,33 @@
-window.addEventListener('load', (event) => {
-  console.log('page is fully loaded');
-
-
-});
+const cityForm = document.querySelector('form');
 
 
 
 
+const updateCity = async (city) => {
+
+    const cityDetails = await getCity(city);
+    const weather = await getWeather(cityDetails.Key);
+
+    return {
+        cityDetails: cityDetails,
+        weather: weather
+    };
+
+};
+
+cityForm.addEventListener('submit', e => {
+    //prevent default action
+    e.preventDefault();
+
+    //get city value
+    const city = cityForm.city.value.trim();
+    cityForm.reset(); //reset city form after submit
 
 
-
-
-
-
+    //update ui with new city
+    updateCity(city)
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => console.log(error));
+})
