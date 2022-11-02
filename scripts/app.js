be7305934c48945447d01c5437cadf5d2f66b017
Alex Vong <avong003@gmail.com>
2022-11-01 17:51:00 -0700

    added date to cards, and function to add forecast cards

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,16 @@
 const body = document.querySelector('body');
 const cityForm = document.querySelector('form');
 
-const card = document.querySelector('.weather-card');
+const card = document.querySelector('.main-card');
 const details = document.querySelector('.weather-details');
 
 const errorMsg = document.querySelector('.error-msg');
 
 const time = document.querySelector('img.time');
 const icon = document.querySelector('.icon img');
+
+const city = document.querySelector('.city');
+const forecastUl = document.querySelector(".forecast ul");
 
 
 
@@ -17,75 +20,129 @@ const icon = document.querySelector('.icon img');
 const updateUI = (data) => {
     const cityDetails = data.cityDetails;
     const weather = data.weather;
-
-
-    //update details template
-
-    // let state = '';
-    // if (cityDetails.AdministrativeArea.CountryID === 'US') {
-    //     // state = cityDetails.AdministrativeArea.EnglishName;
-    //     state = cityDetails.AdministrativeArea.ID;
-    // } else {
-    //     state = cityDetails.Country.EnglishName;
-
-    // }
+    const forecast = data.forecast;
 
     let state = cityDetails.AdministrativeArea.CountryID === 'US' ? cityDetails.AdministrativeArea.ID : cityDetails.Country.EnglishName;
 
-      details.innerHTML = `
-				<h3 class="fourth-level-heading">${cityDetails.EnglishName}, ${state}</h3>
-				<h4 class="body-copy">${weather.WeatherText}</h4>
-				<div class="temp-container fourth-level-heading">
-				    <span class="body-copy">${weather.Temperature.Imperial.Value}</span>
-				    <span class="body-copy">&deg;${weather.Temperature.Imperial.Unit}</span>
-				</div>
 
+    const date = new Date();
+
+    const todayDate = dateFns.format(date, 'dddd');
+    // console.log(todayDate);
+
+
+    // city.innerHTML = `<span>${cityDetails.EnglishName}, ${state}</span>`;
+  card.innerHTML = `
+       <div class="weather-card">
+       		<div class="weather-icon">
+	       		<picture>
+		            <img src="images/icons/${weather.WeatherIcon}.svg" class="time card-img" alt="default city">
+		        </picture>
+	        	<h3 class="second-level-heading">${weather.WeatherText}</h3>
+       		</div>
+
+	        <div class="weather-details">
+    			<p class="fourth-level-heading">${todayDate}</p>
+    			<h2 class="second-level-heading">${cityDetails.EnglishName}, ${state}</h2>
+				<div class="temp-container main-temp">
+				    <span class="body-copy">${weather.Temperature.Imperial.Value}&deg;</span>
+				</div>
+				<div class="hi-low">
+					<span class="body-copy">H: ${forecast.DailyForecasts[0].Temperature.Maximum.Value}&deg;</span>
+					<span class="body-copy">L: ${forecast.DailyForecasts[0].Temperature.Minimum.Value}&deg;</span>
+				</div>
+	        </div>
+        </div>
 	`;
 
-	let timeSrc = `images/icons/${weather.WeatherIcon}.svg`;
+    // let bodyImg = '';
 
-	let bodyImg = null;
+    if (weather.IsDayTime) {
+    	body.classList.remove('night-time');
+    	body.classList.add('day-time');
+    } else {
+    	body.classList.add('night-time');
+    	body.classList.remove('day-time');
+    }
 
-	if(weather.IsDayTime) {
-		// timeSrc = 'images/icons/day-time-mobile.svg';
-		console.log('daytime');
-		bodyImg = 'images/day-time-mobile.svg';
-	} else {
-		// timeSrc = 'images/icons/night-time-mobile.svg';
-		bodyImg = 'images/night-time-mobile.svg';
-		console.log('nighttime');
-	};
-
-	// let bodyImg = weather.IsDayTime ? 'images/day-time-mobile.svg' : 'images/night-time-mobile.svg';
-	// console.log(bodyImg);
-	// console.log(typeof bodyImg);
+    // if (weather.IsDayTime && window.innerWidth < 700 ) {
+    //     bodyImg = 'images/day-time-mobile.svg';
+    // } else if (weather.IsDayTime && window.innerWidth > 700) {
+    //     bodyImg = 'images/day-time-dt.png';
+    // } else if (!weather.IsDayTime && window.innerWidth < 700) {
+    //     bodyImg = 'images/night-time-mobile.svg';
+    // } else {
+    //     bodyImg = 'images/night-time-dt-2.svg';
+    // };
 
 
-
-	// body.style.backgroundImage = "url('../images/icons/day-time-mobile.svg')";
- 	body.style.backgroundImage = `url('${bodyImg}')`;
-
-	time.setAttribute('src', timeSrc);
-
-	
-	// const iconSrc = `images/icons/${weather.WeatherIcon}.svg`;
-
-	// icon.setAttribute('src', iconSrc);
-	
+    // body.style.backgroundImage = `url('${bodyImg}')`;
+    // time.setAttribute('src', timeSrc);
     errorMsg.style.display = 'none';
     card.style.display = 'flex';
 
 
-}
+    if (forecastUl.innerHTML) {
+   		forecastUl.innerHTML = '';
+	};
+
+
+    forecast.DailyForecasts.forEach( day => {
+    	const getToday = new Date(day.Date);
+    	const setToday = (dateFns.format(getToday, 'ddd'));
+
+
+    	//capitlizie first word of each sentence
+		const getDayConditions = day.Day.IconPhrase;
+		const setDayCondition = getDayConditions.split(" ");
+
+		for (let i = 0; i < setDayCondition.length; i++) {
+		    setDayCondition[i] = setDayCondition[i][0].toUpperCase() + setDayCondition[i].substr(1);
+		}
+		const dayCondition = setDayCondition.join(" ");
+
+		const getNightConditions = day.Night.IconPhrase;
+		const setNightCondition = getNightConditions.split(" ");
+
+		for (let i = 0; i < setNightCondition.length; i++) {
+		    setNightCondition[i] = setNightCondition[i][0].toUpperCase() + setNightCondition[i].substr(1);
+		}
+		const nightCondition = setNightCondition.join(" ");
+		
+
+    	const html = `
+	    	<li>
+	    		<h3>${setToday}</h3>
+	    		<section>
+		    		<div class="day">
+		    			<picture>
+		    				<img src="images/icons/${day.Day.Icon}.svg" alt="weather icon">
+		    			</picture>	
+			    		<p>${dayCondition}</p>
+			    		<p class='degrees'>H: ${day.Temperature.Maximum.Value}&deg</p>
+		    		</div>
+		    		 <div class="night">
+		    			<picture>
+		    				<img src="images/icons/${day.Night.Icon}.svg" alt="weather icon">
+		    			</picture>	
+			    		<p>${nightCondition}</p>
+			    		<p class='degrees'>L: ${day.Temperature.Minimum.Value}&deg</p>
+		    		</div>
+	    		</section>
+
+
+	    	</li>
+	    	`;
+    	forecastUl.innerHTML += html;
+    });
+};
 
 
 
 const updateCity = async (city) => {
-
     const cityDetails = await getCity(city);
     const weather = await getWeather(cityDetails.Key);
     const forecast = await getForeCast(cityDetails.Key);
-
     return {
         cityDetails: cityDetails,
         weather: weather,
@@ -118,19 +175,3 @@ cityForm.addEventListener('submit', e => {
 
         });
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
