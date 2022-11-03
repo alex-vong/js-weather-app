c25303876411e6295291422c5f84fe9e8f7c1a09
Alex Vong <avong003@gmail.com>
2022-11-03 11:17:15 -0700

    updated api key and removed s from http request for testing

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,69 +1,50 @@
 
 
-const key ='V2CAw9vUFQjGvDxjx1AAi89epxe6rF39';
+const key ='S3GDCvBxA9xl4gX3UU0GxvEl2eablGC7';
 
 //get weather information
 
 const getWeather = async (id) => {
-
-	const base = 'https://dataservice.accuweather.com/currentconditions/v1/';
-	// const base = 'http://dataservice.accuweather.com/forecasts/v1/daily/5day/';
+	const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
 	const query = `${id}?apikey=${key}`;
-
 	const response = await fetch(base + query);
-
-
 	const data = await response.json();
-
-	// console.log(data[0].WeatherText);
-
+	console.log(data[0]);
 	return data[0];
-
 
 };
 
 const getForeCast = async (id) => {
-
-	// const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
-	const base = 'https://dataservice.accuweather.com/forecasts/v1/daily/5day/';
+	const base = 'http://dataservice.accuweather.com/forecasts/v1/daily/5day/';
 	const query = `${id}?apikey=${key}`;
-
 	const response = await fetch(base + query);
-
-
 	const data = await response.json();
-
-
-
 	return data;
-
-
 };
 
 
 
 // get city information
-const getCity = async (city) => {
 
-	const base = 'https://dataservice.accuweather.com/locations/v1/cities/search';
+//getCity function gets called on line 44 and takes in 'houston' as parameter
+const getCity = async (city) => { //callback asynchronous function taking in 'houston' as 'city' parameter
+	const base = 'http://dataservice.accuweather.com/locations/v1/cities/search'; //saving request url in a variable
 	//when adding query parameters we need to start with ? and use & when we query another one on it
-	const query = `?apikey=${key}&q=${city}`;
-	const response = await fetch(base + query);
-	const data = await response.json();
-	return data[0];
+	const query = `?apikey=${key}&q=${city}`; //assigning api key and city into a query variable
+	const response = await fetch(base + query); //callback function using await operator to get back a promise
+	const data = await response.json(); //convert the promise into json and assign it to a variable 'data';
+	console.log(data);
+	return data[0]; //return data
 
 };
 
-// getCity('houston')
-// 	.then(data => {
+
+// getCity('houston, bc') //call getCity function and pass in 'houston'
+// 	.then(data => { //data gets returned from getCity function
 // 		return getWeather(data.Key);
 // 	}).then(data => {
 // 		console.log(data);
 // 	}).catch (error => console.log(error));
-
-
-
-
 
 
 
