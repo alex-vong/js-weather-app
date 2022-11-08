cfa6a901269ee0035dc035d8ce1939316bfe10c7
Alex Vong <avong003@gmail.com>
2022-11-07 21:15:35 -0800

    updated getCity function

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -28,13 +28,14 @@ const getForeCast = async (id) => {
 
 //getCity function gets called on line 44 and takes in 'houston' as parameter
 const getCity = async (city) => { //callback asynchronous function taking in 'houston' as 'city' parameter
-	const base = 'https://dataservice.accuweather.com/locations/v1/cities/search'; //saving request url in a variable
-	//when adding query parameters we need to start with ? and use & when we query another one on it
-	const query = `?apikey=${key}&q=${city}`; //assigning api key and city into a query variable
-	const response = await fetch(base + query); //callback function using await operator to get back a promise
-	const data = await response.json(); //convert the promise into json and assign it to a variable 'data';
 
-	return data[0]; //return data
+		const base = 'https://dataservice.accuweather.com/locations/v1/cities/search'; //saving request url in a variable
+		//when adding query parameters we need to start with ? and use & when we query another one on it
+		const query = `?apikey=${key}&q=${city}`; //assigning api key and city into a query variable
+		const response = await fetch(base + query); //callback function using await operator to get back a promise
+		const data = await response.json(); //convert the promise into json and assign it to a variable 'data';
+
+		return data[0]; //return data
 
 };
 
